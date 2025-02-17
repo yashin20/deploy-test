eb7742fc1c9065d03a67c421c11ff8cc5c2f9c08
JinyeungKim <92693776+yashin20@users.noreply.github.com>
2025-02-18 01:42:04 +0900

    Delete front-end-server/config.js

--- front-end-server/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    // 배포 주소에 따른 유지보수
-    // baseUrl : "http://localhost:8080"
-
-    //'backend-application-instance' 서버 ip 주소 + 포트번호
-    baseUrl : "http://158.247.249.116:8080"
-}
