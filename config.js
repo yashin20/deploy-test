a6ff603707752338d2ade6290299efa9765cbf34
JinyeungKim <92693776+yashin20@users.noreply.github.com>
2025-02-26 22:58:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const config = {
     // baseUrl : "http://localhost:8080"
 
     //'backend-application-instance' 서버 ip 주소 + 포트번호
-    baseUrl : "http://158.247.249.116:8080"
+    baseUrl : "http://{backend-server-ip-address}:8080"
 }
